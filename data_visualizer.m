fc468f938ff91e2128db045a5744993bdf11b608
Askari Husaini <ahusaini@umich.edu>
2024-10-30 01:46:09 -0400

    Created visualizer app; Log file inputting and reformatting works

--- data_visualizer.m
+++ data_visualizer.m
@@ -35,3 +35,33 @@ if answer == "Yes"
     writetable(data, strcat(location, file));
     disp('Finished reformatting')
 end
+
+%% GUI
+
+screenSize = get(0,'ScreenSize');
+GUI_WIDTH = 400;
+GUI_HEIGHT = 600;
+
+GUI.f = figure("Name", "2024 MRacing Driver Data Visualizer", "NumberTitle","off", ...
+    "MenuBar","none", "ToolBar","none", ...
+    "Units","pixels", "Position",[100,screenSize(4)-GUI_HEIGHT - 100,GUI_WIDTH,GUI_HEIGHT]);
+
+GUI.run = uicontrol('Style','pushbutton', 'String','Visualize Data',...
+    'Units','pixels', 'Position',[20, 20, 100, 30], ...
+    'Callback', @(src, event) run_visualizer(src, event, GUI));
+
+function run_visualizer(src, event, GUI)
+    %disp(get(GUI.speedBox, 'Value'))
+end
+
+% % Create yes/no checkboxes
+% GUI.c(1) = uicontrol('style','checkbox','units','pixels',...
+%                 'position',[10,30,50,15],'string','yes');
+% GUI.c(2) = uicontrol('style','checkbox','units','pixels',...
+%                 'position',[90,30,50,15],'string','no');    
+% % Create OK pushbutton   
+% GUI.p = uicontrol('style','pushbutton','units','pixels',...
+%                 'position',[40,5,70,20],'string','OK');
+% set(GUI.p, 'callback', @(src, event) p_call(src, event, GUI));
+% 
+% Pushbutton callback
