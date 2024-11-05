8da50b51a030d32f6d1a6355289c5748c909c4bb
Askari Husaini <ahusaini@umich.edu>
2024-11-05 18:24:17 -0500

    Organized code a bit and started messing with plotting in testing.m

--- testing.m
+++ testing.m
@@ -1,24 +1,46 @@
-%% File input
-disp("Select log file")
-[file, location] = uigetfile('*.txt');
+% %% File input
+% disp("Select log file")
+% [file, location] = uigetfile('*.txt');
+% 
+% % Check if file exists
+% if isequal(file, 0)
+%     disp('No file selected. Exiting...')
+%     return
+% end
+% 
+% data = readtable([location, file]);
 
-% Check if file exists
-if isequal(file, 0)
-    disp('No file selected. Exiting...')
-    return
-end
-    
+figureColor = [0,0,0]; % Black figure
+subplotColor = [0.1,0.1,0.1]; % Dark gray graph background
+set(groot,{'DefaultAxesXColor','DefaultAxesYColor','DefaultAxesZColor'},{'w','w','w'}) % White axes
 
-data = readtable([location, file]);
+f1 = figure('color', figureColor);
+sgtitle("Lap Analysis (1)")
 
-                lap_times = data{:,"Dash_3_Lap_Time_s_"};
-                % Find smallest non-zero lap time
-                lapf_t = min(lap_times(lap_times>0));
-                if isempty(lapf_t)
-                    lapf_t = 0;
-                end
-                % Grab row of lapf_t and use it to find lapf number
-                lapf = data{lap_times==lapf_t, "Dash_3_Lap_Number_None_"}(1);
+%axis vars
+axs_length = 5;
+axs_width = 4;
+NUM_VARS = 5; % Number of variables we are plotting
+
+axisHeight = 1/(NUM_VARS + 0.1);
+
+axisPos = [1-axisHeight, 1-(2*(axisHeight-.001)), 1-(3*(axisHeight-.001)), 1-(4*(axisHeight-.001)), 1-(5*(axisHeight-0.001))] - 0.03;
+
+axs_SPEED = subplot(axs_length,axs_width, [1:3], 'color', subplotColor, 'Position', [0.03,axisPos(1),0.65,axisHeight]);
+xticks([]);
+yticks([]);
+ylabel("Speed(mph)")
+
+
+% lap_times = data{:,"Dash_3_Lap_Time_s_"};
+% % Find smallest non-zero lap time
+% lapf_t = min(lap_times(lap_times>0));
+% if isempty(lapf_t)
+%     lapf_t = 0;
+% end
+% % Grab row of lapf_t and use it to find lapf number
+% lapf = data{lap_times==lapf_t, "Dash_3_Lap_Number_None_"}(1);
+% f = msgbox("Operation Completed");
 
 
 % data = readtable([location, file]);
