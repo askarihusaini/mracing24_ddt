24e1d159ac67604d84e6ceedae65347e2d9acb01
Askari Husaini <ahusaini@umich.edu>
2024-11-18 23:48:55 -0500

    Selection boxes for variables made; graphs plotting nicely and info box working in testing file

--- testing.m
+++ testing.m
@@ -15,79 +15,86 @@
 clear
 close all
 
+% The below code will all get replaced by actual data
 timei = 0;
 timef = 10;
 x = timei:.01:timef;
-data = [sin(x) ; cos(x) ; tan(x) ; x/10 ; x.^2/10 ; x.^3/10 ; 0*x + 0.5 ; -x/10];
-%data = [sin(x); cos(x); tan(x); x/10; x.^2/10; (x.^3 - x)/10; 0.5; -x/10];
+data1 = [sin(x) ; cos(x) ; tan(x) ; x/10 ; x.^2/10 ; x.^3/10 ; 0*x + 0.5 ; -x/10];
+data2 = data1 - 1;
 
-axis_names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-selected_vars = [1,2,3,4,5];
+axis_names = ["Var A", "Var B", "Var C", "Var D", "Var E", "Var F", "Var G", "Var H"];
+selected_vars = [1,2,3,4,5,6,7,8];
+
 NUM_VARS = size(selected_vars,2);
-plots = zeros(NUM_VARS);
-
-% Manual
-% AXIS_LENGTH = 5;
-% AXIS_HEIGHT = 4;
-% BUFFER = 0.025;
-% PROP_WIDTH = 0.5;
-% axes = zeros(NUM_VARS);
-% for i = 1:NUM_VARS
-%     axes(selected_vars(i)) = plot(AXIS_LENGTH, AXIS_HEIGHT, (i-1)*AXIS_HEIGHT+1, ...
-%     'color', subplotColor, 'Position', [BUFFER, 1 - i * (1/NUM_VARS - BUFFER), PROP_WIDTH, .1]);
-%     %[BUFFER, 1 - i * (1/NUM_VARS - BUFFER), PROP_WIDTH, 1/NUM_VARS - BUFFER]
-%     xticks([]); % remove markings
-%     yticks([]);
-%     ylabel(strcat("Graph ", string(axis_names(selected_vars(i)))))
-% end
+axes = zeros(NUM_VARS);
 
 figureColor = [0,0,0];
 set(groot,{'DefaultAxesXColor','DefaultAxesYColor','DefaultAxesZColor'},{'w','w','w'})
 
 f1 = figure('color', figureColor);
-sgtitle("Lap Analysis (1)") 
 
-% Create tiled layout with three vertical sections
+% Tiled layout
 T = tiledlayout(1,3, "TileSpacing", "compact", "Padding", "compact");
 
-% First subtile spans two vertical sections, with inner horizontal sections
-t1 = tiledlayout(T, "vertical", "TileSpacing", "none", "Padding", "compact");
+% Tile 1: Tiled layout: Graphs of variables
+t1 = tiledlayout(T, "vertical", "TileSpacing", "compact", "Padding", "compact");
 t1.Layout.Tile = 1;
 t1.Layout.TileSpan = [1,2];
-% Fill each inner horizontal section with a graph
+% Subtiles: Individual graphs of variables
 for i = 1:NUM_VARS
     nexttile(t1)
-    plots(i) = plot(x, data(i,:), 'r');
+    curr = axes(i);
+    hold on
+    curr(1) = plot(x, data1(i,:), "Color", [1,0,0]);
+    curr(2) = plot(x, data2(i,:), "Color", [0,0.5,1]);
+    hold off
     set(gca,'Color', [.1, .1, .1])
     xticks([])
     yticks([])
-    ylabel("Var. " + axis_names(selected_vars(i)))
+    ylabel(axis_names(selected_vars(i)))
 end
 xticks();
 xlabel("time (s)")
 
-% Second subtile spans the third vertical section, with five horizontal
-% sections
-t2 = tiledlayout(T, 5,1, "TileSpacing", "compact", "Padding", "compact");
+% Tile 2: Tiled layout: Infobox and map
+t2 = tiledlayout(T, 2,1, "TileSpacing", "compact", "Padding", "none");
 t2.Layout.Tile = 3;
 
-% The info readout spans three of the inner horizontal sections
-nexttile(t2, 1, [3,1])
-text(0,0.1, sprintf( ...
-    "Time\nSpeed\nThrottle\netc..." ...
-    ), "Color", "w", "FontSize", 10)
-
+% Subtile A: Tiled layout: 2 columns for track data of both laps
+nexttile(t2, 1, [1,1])
 Ax = gca;
-Ax.Visible = 0; % Make current axis invisible (just display text)
+Ax.Visible = 0; % Make axes invisible for text display
+t2a = tiledlayout(t2, 1,2, "TileSpacing", "none", "Padding", "none");
+t2a.Layout.Tile = 1;
 
-% infoBox_str = {['Time: ',num2str(0)], ['Speed: ', num2str(0)], ['Throttle: ', num2str(0)], ['Brake: ', num2str(0)], ['RPM: ', num2str(0)], ['GForce: ', num2str(0)]};
-% infoBox_dim = [.915 .375 .75 .2];
-% infoBox_color = [1 1 1];
-% infoBox = annotation(f1, 'textbox',infoBox_dim,'String',infoBox_str,'FitBoxToText','on', 'color', infoBox_color, 'edgecolor', infoBox_color, 'FontSize', 11);
+% Subsubtile i & ii: Lap data of track 1 & 2
+select_t = 0; % Selected time by user
+for i = [1,2]
+    nexttile(t2a, i, [1,1])
+    p = plot(x, 0);
 
-% The map spans the fourth and fifth horizontal section
-nexttile(t2, 4, [2,1])
+    ax = gca;
+    ax.XTick = [];
+    ax.YTick = [];
+    ax.Color = [.1,.1,.1];
+    ax.Box = "on";
+
+    infoBox_string = "Lap " + i + ":\n@ t = " + select_t + "\n";
+    infoBox_data = zeros(NUM_VARS);
+    for j = 1:NUM_VARS
+        infoBox_string = infoBox_string + axis_names(selected_vars(j)) + ": " + infoBox_data(j) + "\n";
+    end
+
+    xlim(xlim+[-10,10]);ylim(ylim+[-10,10]); % use xlim and ylim to fix location  
+    text(min(xlim)+1, max(ylim)-.1, sprintf(infoBox_string), 'Horiz','left', 'Vert','top', ...
+        "Color", "w", 'fontsize',8,'fontunits','normalized')
+
+end
+
+% Subtile B: Map
+nexttile(t2, 2, [1,1])
 plot(x, sin(x))
+
 
 
 %% old
